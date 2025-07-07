520a9ebf0e8dbacca0b4fdf00957d68daf85b500
Ishan Singh <ishan2004.official@gmail.com>
2025-07-07 16:10:17 +0530

    Separated ShowDetails component into smaller, readable components.

--- src/components/Home/DisplayResults/ShowDetails/index.jsx
+++ src/components/Home/DisplayResults/ShowDetails/index.jsx
@@ -1,14 +1,10 @@
-import { FALLBACK_IMAGE_URL } from "constants/constant";
-
-import { PageLoader, Tooltip } from "components/commons";
+import { PageLoader } from "components/commons";
+import Body from "components/Home/DisplayResults/ShowDetails/Body";
+import Header from "components/Home/DisplayResults/ShowDetails/Header";
 import { useOmdbShow } from "hooks/reactQuery/useOmdbApi";
 import { t } from "i18next";
-import { Rating } from "neetoicons";
 import { Modal } from "neetoui";
 import useFavouritesStore from "stores/useFavouritesStore";
-import { fallbackHelper } from "utils/fallbackHelper";
-
-import Label from "./Label";
 
 const ShowDetails = ({ showId, isOpen, setIsOpen }) => {
   const { favourites, toggleFavourite } = useFavouritesStore();
@@ -27,16 +23,6 @@ const ShowDetails = ({ showId, isOpen, setIsOpen }) => {
     { content: showDetails?.imdbRating, label: t("showDetails.rating") },
     { content: showDetails?.rated, label: t("showDetails.rated") },
   ];
-
-  const handleTooltipContent = () => {
-    if (showId in favourites) {
-      return t("favourites.removeWarning");
-    }
-
-    return t("favourites.add");
-  };
-
-  const handleStarFill = () => (showId in favourites ? "gold" : "none");
 
   const handleToggleFavourite = () => {
     toggleFavourite({
@@ -60,56 +46,17 @@ const ShowDetails = ({ showId, isOpen, setIsOpen }) => {
         }}
       >
         <div className="flex flex-col items-center p-6">
-          <div className="Header mb-3 self-start">
-            <div className="Title">
-              <h1 className="text-[32px] font-bold">
-                {showDetails?.title}
-                <Tooltip
-                  placement="right"
-                  tooltipContent={handleTooltipContent()}
-                >
-                  <div
-                    className="mb-1.5 ml-6 inline cursor-pointer"
-                    onClick={handleToggleFavourite}
-                  >
-                    <Rating className="mb-1 inline" fill={handleStarFill()} />
-                  </div>
-                </Tooltip>
-              </h1>
-            </div>
-            <div className="Genre flex flex-row gap-2">
-              {showDetails?.genre?.split(",").map((genre, index) => (
-                <span
-                  className="rounded-full bg-gray-200 px-3 py-1 text-sm text-gray-700"
-                  key={index}
-                >
-                  {genre.trim()}
-                </span>
-              ))}
-            </div>
-          </div>
-          <div className="Body mt-4 flex min-h-[350px] w-full flex-row gap-6">
-            <div className="Poster w-2/5">
-              <div className="w-max rounded-2xl border-8 border-gray-200 shadow-2xl">
-                <img
-                  alt={showDetails?.title}
-                  className="rounded-lg"
-                  src={fallbackHelper(showDetails?.poster, FALLBACK_IMAGE_URL)}
-                  onError={({ target }) => {
-                    target.src = FALLBACK_IMAGE_URL;
-                  }}
-                />
-              </div>
-            </div>
-            <div className="Details flex w-3/5 flex-col justify-between">
-              <p className="text-gray-600">
-                {fallbackHelper(showDetails?.plot, t("error.noPlotDetails"))}
-              </p>
-              {labels.map((item, index) => (
-                <Label content={item.content} key={index} label={item.label} />
-              ))}
-            </div>
-          </div>
+          <Header
+            {...{ showDetails, favourites, handleToggleFavourite, showId }}
+          />
+          <Body
+            {...{
+              title: showDetails?.title,
+              poster: showDetails?.poster,
+              plot: showDetails?.plot,
+              labels,
+            }}
+          />
         </div>
       </Modal>
     )
